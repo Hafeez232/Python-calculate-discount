0a2eec9c56ec95362cc63b5bed4c6fb596fe9788
Hafeez <58874280+Hafeez232@users.noreply.github.com>
2021-02-21 17:04:44 +0800

    Update PYTHON DISCOUNT.py

--- PYTHON DISCOUNT.py
+++ PYTHON DISCOUNT.py
@@ -6,35 +6,37 @@ jum_pembelian = float(input("Masukkan jumlah pembelian: RM "))
 
 # tentukan jumlah pembelian
 # proses
-if jum_pembelian == "0":
-    exit
+if  jum_pembelian <= 99:
+    print("\nDiskaun ialah 0%")
+    diskaun = 0.00
+    jum_diskaun = jum_pembelian * diskaun
+    print("\nJumlah diskaun: RM", round(jum_diskaun))
+    
 elif jum_pembelian >= 400:
     print("\nDiskaun ialah 25%")
     diskaun = 0.25
     jum_diskaun = jum_pembelian * diskaun
     print("\nJumlah diskaun: RM", round(jum_diskaun))
+    
 elif jum_pembelian >= 301:
     print("\nDiskaun ialah 20%")
     diskaun = 0.20
     jum_diskaun = jum_pembelian * diskaun
     print("\nJumlah diskaun: RM", round(jum_diskaun))
+    
 elif jum_pembelian >= 201:
     print("\nDiskaun ialah 15%")
     diskaun = 0.15
     jum_diskaun = jum_pembelian * diskaun
     print("\nJumlah diskaun: RM", round(jum_diskaun))
-elif jum_pembelian >= 100:
+    
+else:
+    jum_pembelian >= 100
     print("\nDiskaun ialah 10%")
     diskaun = 0.10
     jum_diskaun = jum_pembelian * diskaun
     print("\nJumlah diskaun: RM", round(jum_diskaun))
-
-else:
-    jum_pembelian <= 99
-    print("\nDiskaun ialah 0%")
-    diskaun = 0.00
-    jum_diskaun = jum_pembelian * diskaun
-    print("\nJumlah diskaun: RM", round(jum_diskaun))
+    
     
 jum_pembelian = jum_pembelian - jum_diskaun
 
